ecb82c7c8ca5e38845684727c6fd80936ceccdf2
Aisha Sabrie <sabriealt@gmail.com>
2021-11-29 11:37:44 -0500

    removed test code and assertEquals function definition  with an export of assertEqual

--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😃😃😃Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤢🤢🤢Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 let head = function(array) {
   return array[0];
 };
-//test code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), 6);
+
+module.exports = head;
