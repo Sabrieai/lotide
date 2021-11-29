4280b79011e09d9f55d93ec0b4ca69929224ed41
Aisha Sabrie <sabriealt@gmail.com>
2021-11-29 16:18:28 -0500

    removed tests and function definition of eqArrays, added export of assertArraysEqual and required equalArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,8 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(array1, array2) {
   console.log(eqArrays(array1, array2) === true ? "😃" : "🤢");
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-console.log(assertArraysEqual([2, 2, 3], [2, 2, 3]));
+module.exports = assertArraysEqual;
 
