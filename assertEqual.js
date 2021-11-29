c9f00a8f73ffe8eceeee7a5e0a811f1382d78a64
Aisha Sabrie <sabriealt@gmail.com>
2021-11-29 10:10:09 -0500

    removed test code and added module.exports

--- assertEqual.js
+++ assertEqual.js
@@ -7,12 +7,4 @@ const assertEqual = function(actual, expected) {
 };
 
 
-// Comparing identical strings
-assertEqual("Lighthouse Labs", "Bootcamp");
-//Comparing non-identical strings
-assertEqual("Bootcamp", "Bootcamp");
-//Comparing identical numbers
-assertEqual(1, 1);
-//Comparing non-identical numbers
-assertEqual(1, 2);
-
+module.exports = assertEqual;
