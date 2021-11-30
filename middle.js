eaa39da70c0fcf3b048e474a687ca9e18dbad25a
Aisha Sabrie <sabriealt@gmail.com>
2021-11-29 19:55:16 -0500

    removed the require for eqArrays and assertArraysEqual becuase we are no longer testing here, also removed comments

--- middle.js
+++ middle.js
@@ -1,9 +1,3 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-
-//Implement middle which will take in an array and return the middle-most element(s) of the given array.
-// be able to count the elements and push the middle one
-//one or two elements: Return an empty array.
 
 const middle = function(arr) {
   const results = [];
@@ -11,7 +5,6 @@ const middle = function(arr) {
     return results;
   }
   if (arr.length % 2 === 0) {
-    // code for when length is even
     let value1 = Math.floor(Number(arr.length / 2));
     results.push(arr[value1 - 1]);
     results.push(arr[value1]);
