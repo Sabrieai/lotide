29141345f8b86f917fac7371f2e0e54078e1ed76
Aisha Sabrie <sabriealt@gmail.com>
2021-11-29 20:11:44 -0500

    removed the require for eqArrays and assertArraysEqual becuase we are no longer testing here, also required chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,23 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const chai = require('chai');
+const assert = chai.assert;
 const middle = require("../middle");
+
+describe("#middle", () => {
+  it("returns [7, 3] for [1, 7, 3, 5]", () => {
+    assert.deepEqual(middle([1, 7,3, 5]), [7, 3]);
+  });
+
+  it("returns [7] for [1, 7, 3]", () => {
+    assert.deepEqual(middle([1, 7,3]), [7]);
+  });
+
+  it("returns [] for [8]", () => {
+    assert.deepEqual(middle([8]), []);
+  });
+
+  it("returns [] for ['dont', 'print']", () => {
+    assert.deepEqual(middle(['dont', 'print']), []);
+  });
+});
 
 console.log(middle([1,7,3,5,]));
