4a5dad77a8dcfadadae21fb5040d6b8e0f1a775f
Aisha Sabrie <sabriealt@gmail.com>
2021-11-29 19:44:16 -0500

    used mocha and chai to test tail, and removed both test code and require of assertEqual for chai.assert

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,25 @@
-const assertEqual = require('../assertEqual');
+const chai = require('chai');
+const assert = chai.assert;
+
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns [1, 2] for [0, 1, 2]", () => {
+    assert.deepEqual(tail([0, 1, 2]), [1, 2]);
+  });
+  
+  it("returns [] for [2] ", () => {
+    assert.deepEqual(tail([2]), []);
+  });
+  
+  it("returns ['it', 'works!'] for [' I dont know if', 'it', 'works!'] ", () => {
+    assert.deepEqual(tail([' I dont know if', 'it', 'works!']), ['it', 'works!']);
+  });
+
+  it("returns ['it', 'works!'] for [' I dont know if', 'it', 'works!'] ", () => {
+    assert.deepEqual(tail([' I dont know if', 'it', 'works!']), ['it', 'works!']);
+  });
+});
+
+
+
